26f1ffa51a5b8b252dbfb3b868878f6031900bf7
Jennifer Silva <jenni75dr@gmail.com>
2020-07-27 14:00:20 -0300

    Exercicios prontos

---
+++ semana3/variaveis/index.js
@@ -0,0 +1,87 @@
+/*a = 10
+b = 10
+
+console.log(b)
+10
+
+b = 5
+console.log(a, b)
+10, 5
+*/
+
+
+/* a = 10
+b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+10 ,10 ,10 
+*/
+
+/*Exercicio 1*/
+
+ let armazenarNome
+ console.log(typeof(armazenarNome));
+
+ let armazenarIdade
+ console.log(typeof(armazenarIdade))
+ 
+ /* O undefined aparece quando não declaramos o valor da variavel*/
+
+ const nome= "Jennifer"
+ console.log(nome);
+ let idade= "32"
+ console.log(idade)
+
+ /*Quando declaramos as variaveis o console imprime seus recepctivos valores , tipo string e numbers. */
+
+ console.log ("Olá Jennifer você tem 32 anos")
+
+/*Exercicio 2*/
+
+console.log ("Qual a sua cidade?")
+let cidade ="Piracicaba"
+console.log (cidade)
+
+console.log("Qual sua comida favorita?")
+let comida="Pizza"
+console.log (comida)
+
+console.log("Qual a sua cor favorita?")
+let cor ="Vermelho"
+console.log (cor)
+
+console.log("Qual o seu animal favorito?")
+let animal= "Cachorro"
+console.log (animal)
+
+console.log("Qual a sua altura?")
+let altura= "1.60"
+console.log (altura)
+
+/*Exercicos 3*/
+let comidasFavoritas = ["macarrão", "maionese", "pizza", "pastel", "lanche"]
+console.log (comidasFavoritas [0],comidasFavoritas[1], comidasFavoritas[2], comidasFavoritas[3],comidasFavoritas[4],)
+console.log("Essas são as minhas comidas preferidas: ")
+console.log(comidasFavoritas[0])
+console.log(comidasFavoritas[1])
+console.log (comidasFavoritas[2])
+console.log (comidasFavoritas[3])
+console.log(comidasFavoritas[4])
+
+let suaComidaFavorita=["Polenta", "maionese", "pizza", "pastel", "lanche"]
+console.log ("Qual a sua comida preferida")
+console.log(suaComidaFavorita[0])
+console.log(suaComidaFavorita[1])
+console.log (suaComidaFavorita[2])
+console.log (suaComidaFavorita[3])
+console.log(suaComidaFavorita[4])
+/*Exercicio 4*/
+
+let perguntasPessoais = ["Você dorme tarde toodos os dias?", "Você tem passarinhos em casa?", "Você tem olhos verde? "]
+let respostas= ["true","false", "true",]
+console.log(perguntasPessoais[0],respostas[0])
+console.log(perguntasPessoais[1],respostas[1])
+console.log(perguntasPessoais[2],respostas[2])
