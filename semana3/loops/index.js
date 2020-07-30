791a588a61f274c2625b76d3a8ade383114eb66f
Jennifer Silva <jenni75dr@gmail.com>
2020-07-30 16:35:15 -0300

    Exercícios incompleto

---
+++ semana3/loops/index.js
@@ -0,0 +1,41 @@
+// Exercício 1
+
+/* Ele faz uma condição de continuação
+ depois de ação repetida imprime o incremento que é o (i) , o resultado vai ser (10).
+*/
+
+// Exercício 2
+
+/* a- Ele imprime todos os números maiores que 18.
+   b- Não o for of não pode usar  o indice.
+   */
+
+   // Exercíco 3
+
+   //a
+
+    const arrayOriginal = [10,15,25,30,35,50,65,70,72,80]
+
+   for(let i= 0; i < arrayOriginal . length; i++){
+     console.log(arrayOriginal[i])
+   }
+ 
+   //b
+   const arrayNum = [10,15,25,30,35,50,65,70,72,80]
+
+   for(let i= 0; i < arrayNum . length; i++){
+     console.log(arrayNum[i]/10)
+   }
+ 
+
+   //c
+   
+   let novoArray = [10,15,25,30,35,50,65,70,72,80]
+     console.log(novoArray[0],novoArray[3],novoArray[5],
+        novoArray[7],novoArray[8],novoArray[9])
+
+        //d
+  
+
+//e
+
