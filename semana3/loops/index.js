50c8f6568614ad452c2492e4b23be80cb83b6ac2
Jennifer Silva <jenni75dr@gmail.com>
2020-07-30 17:43:19 -0300

    Exercícios 1 ao 3 pronto

--- semana3/loops/index.js
+++ semana3/loops/index.js
@@ -30,12 +30,39 @@
 
    //c
    
-   let novoArray = [10,15,25,30,35,50,65,70,72,80]
-     console.log(novoArray[0],novoArray[3],novoArray[5],
-        novoArray[7],novoArray[8],novoArray[9])
+   let numbers = [1,2,20,44,45,55,58,80]
+   let novoArray= []
 
+   for (num of numbers){
+       if (num %2 === 0){
+           novoArray= num
+       }
+       console.log(novoArray);
+   }
         //d
+
+        let array= [21,62,1,5,70]
+        for(i =0 ; i < array.length; i++){
+       console.log(`O elemento do index ${i} é o número ${array[i]}`)
+}
   
 
 //e
 
+const number = [10,15,25,30,35,50,65,70,72,80]
+  let valorMaximo = 0
+  let valorMinimo = 90
+  
+  for (i = 0; i< number.length; i++){
+  if(number[i]> valorMaximo){
+  valorMaximo =number[i];
+  }
+  }
+  
+  for(i = 0; i <number.length; i++){
+    if(number[i]<valorMinimo){
+      valorMinimo=number[i]
+  
+    }
+  }
+  console.log(`O maior numero é ${valorMaximo} e o menor número é ${valorMinimo}`)
