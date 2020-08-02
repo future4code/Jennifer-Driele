4f1519bedce11708b6b29b1135939d5b1ffd9d1c
Jennifer Silva <jenni75dr@gmail.com>
2020-08-02 15:18:35 -0300

    Exercício pronto e parte do desafio pronto

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -11,3 +11,57 @@
  * 
  */
 
+//  if(confirm("Que iniciar uma nova rodada")){
+//     const cartaUsuario = [comprarCarta(),comprarCarta()]
+//     const cartaComputador =[comprarCarta(),comprarCarta()]
+    
+//     for(let i of cartaUsuario){
+//        if(cartaUsuario[i] === "A"){
+//           i++
+//           if(cartaUsuario[i] === "A"){
+//              cartaUsuario = [comprarCarta(), comprarCarta()]
+//             }else{
+//             }
+//           } for(let i of cartaComputador){
+//                if(cartaComputador[i] === "A"){
+//                   i++
+//                   if(cartaComputador[i] === "A"){
+//                      cartaComputador = [comprarCarta(),comprarCarta()]
+//                      }else{
+//                      }
+//                   }
+//                } 
+//             } 
+//        }
+//        if(confirm("Suas cartas são", cartaUsuario[0].texto , cartaUsuario.texto, "A carta revelada do usuario ",  cartaUsuario[0] + "\n" + "Desejo comprar mais carta?")){
+//           const maisCarta = comprarCarta()
+//           cartaUsusario.push(maisCarta)
+//           let resultadoUsuario = 0 
+//           let resultadoComputador = cartaComputador.valor[0] + cartaComputador.valor[1] + CartaComputador[2] + carta.valor[3]
+
+//           for(let i of cartaUsuario){
+//              resultado += i.valor;
+//              if(resultadoUsuario < 21){
+//                 let resultadoUsuarioText = ""
+//                 for(let a of cartaUsuario){
+//                    resultadoUsuarioText += a.texto + ""
+//                 }
+//                 if(confirm("Suas cartas são", resultadoUsuarioText, "A carta revelada do computador", cartaComputador[1] + "\n" + "Deseja comprar mais carta?")){
+
+//                 }
+//             }
+//           }
+//          }
+
+
+//          switch(resultadoUsuario){
+//             case resultadoUsuario ===21 && resultadoUsuario > resultadoComputador:
+//                alert("Suas cartas são", cartaUsuario.texto, "Sua pontuação é", resultadoUsuario, "\n .A pontuação é ", cartaComputador.texto, " A pontuação é " , resultadoComputador,".\n O computador ganhou!")
+//                break
+//                case resultadoUsuario > 21 && resultadoUsuario > resultadoComputador:
+//                   alert("Suas carta são", cartaUsuario. texo, "Sua pontuação é", resultadoUsuario,"\n .As cartas do computador são" , cartaComputador.texto,"A pontuação do computador é", resultadoComputador, ".\n O computador ganhou!")
+//                   break
+//          }
+       
+   
+
--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -17,24 +17,24 @@
 
 // console.log("Bem Vindo ao jogo de BlackJack !")
 // if(confirm("Quer iniciar uma nova rodada?")){
-//    const carta1 = comprarCarta()
-//    const carta2 = comprarCarta()
-//    const carta3 = comprarCarta()
-//    const carta4 = comprarCarta()
-//    let resultadoUsuario= carta1.valor + carta2.valor
-//    let resultadoComputador = carta3.valor + carta4.valor
-//    console.log("Usuario - Cartas:" , carta1.texto , carta2.texto,"- Pontuação  " , resultadoUsuario)
-//        console.log("Computador - Cartas:" , carta3.texto , carta3.texto,"- Pontuação  " , resultadoComputador)
-//        if(resultadoUsuario > resultadoComputador){
-//              console.log("O usuario ganhou!")
-//        }else if(resultadoUsuario < resultadoComputador){
-//           console.log("O computador ganhou!")
-//        }else{
-//           console.log("Empatou!")
-//        }
-// } else{
-//    console.log("O jogo acabou")
-// }  
+//      const carta1 = comprarCarta()
+//      const carta2 = comprarCarta()
+//      const carta3 = comprarCarta()
+//      const carta4 = comprarCarta()
+//      let resultadoUsuario= carta1.valor + carta2.valor
+//      let resultadoComputador = carta3.valor + carta4.valor
+//      console.log("Usuario - Cartas:" , carta1.texto , carta2.texto,"- Pontuação  " , resultadoUsuario)
+//          console.log("Computador - Cartas:" , carta3.texto , carta3.texto,"- Pontuação  " , resultadoComputador)
+//          if(resultadoUsuario > resultadoComputador){
+//                console.log("O usuario ganhou!")
+//          }else if(resultadoUsuario < resultadoComputador){
+//             console.log("O computador ganhou!")
+//          }else{
+//             console.log("Empatou!")
+//          }
+// } else{  
+//      console.log("O jogo acabou")
+//   }  
 
        
 
