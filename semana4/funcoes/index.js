70ff297c0f6b0a2f201b855647c26162620c7fb6
Jennifer Silva <jenni75dr@gmail.com>
2020-08-03 22:16:22 -0300

    Exercicios pronto e parte do desafio pronto

--- semana4/funcoes/index.js
+++ semana4/funcoes/index.js
@@ -18,78 +18,136 @@
 
   // Exercício 4
 
-  function minhaMensagem(){
-    console.log(" Eu sou Jennifer", "tenho 32 anos", "Moro em Piraciacaba e sou estudante. ")
-     }
-     minhaMensagem()
+//   function minhaMensagem(){
+//     console.log(" Eu sou Jennifer", "tenho 32 anos", "Moro em Piraciacaba e sou estudante. ")
+//      }
+//      minhaMensagem()
 
-  function minhaMensagem1(nome,idade,cidade,estudante){
-      if(estudante === false){
-          console.log(`Èu sou ${nome},tenho ${idade} anos, moro em ${Cidade} e não sou estudante.`)
-      }else if(estudante === true){
-          console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e  sou estudante.`)
-      }
-}
- const resultado = minhaMensagem1("Jennifer", 32 ,"Piracicaba - Sp", true)
+//   function minhaMensagem1(nome,idade,cidade,estudante){
+//       if(estudante === false){
+//           console.log(`Èu sou ${nome},tenho ${idade} anos, moro em ${Cidade} e não sou estudante.`)
+//       }else if(estudante === true){
+//           console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e  sou estudante.`)
+//       }
+// }
+//  const resultado = minhaMensagem1("Jennifer", 32 ,"Piracicaba - Sp", true)
 
 
- // Exercício 5
-//a
+//  // Exercício 5
+// //a
 
-  function doisNumeros (num1, num2){
-      const soma = num1 + num2
-      console.log(soma)
-  }
-  doisNumeros(10 ,15)
+//   function doisNumeros (num1, num2){
+//       const soma = num1 + num2
+//       console.log(soma)
+//   }
+//   doisNumeros(10 ,15)
 
-  //b
+//   //b
 
-  function doisNumeros1 ( number1 ,number2){
-      if(number1 >= number2){
-          console.log( true)
-      }else if( number1 <= number2){
-          console.log(false)
-      }
-  }
-  doisNumeros1(10,5)
+//   function doisNumeros1 ( number1 ,number2){
+//       if(number1 >= number2){
+//           console.log( true)
+//       }else if( number1 <= number2){
+//           console.log(false)
+//       }
+//   }
+//   doisNumeros1(10,5)
 
-  //c
+//   //c
+//  function minhaMensagem10(string){
+//      for (i = 0; i < 10; i++) {
+//        console.log(i , string);
+// }
+//  }
+//  let frase = "O dia está lindo"
+//  const frase2 = minhaMensagem10(frase)
 
-  let minhaMensagem2
-for (minhaMensagem2 = 0; minhaMensagem2 < 10; minhaMensagem2++) {
-  // Executa 10 vezes, com os valores de passos de 0 a 9.
-  console.log('Olá dia está lindo');
-}
+// //Exercício 6
 
-//Exercício 6
+// // a
+// function verificaNumero(verificaArray){
+//     for(let elemento of verificaArray){ 
+//     console.log(elemento)
+// }
+// return verificaArray.length
+// }
+// const arrayNumber = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+// const resultado1 = verificaNumero(arrayNumber)
+// console.log(resultado)
 
-// b-
-function numeroPar(number){
-    if(number % 2 === 0){
-        return true
-    } else if ( number % 2 === 1){
-        return false
-    }
-}
 
-let inpuNumber=prompt("Digite um número")
-const passo1 = numeroPar(inpuNumber)
-console.log(`O número ${inputNumber} é part? Resposta: ${passo1}`)
+
+// // b-
+// function numeroPar(number){
+//     if(number % 2 === 0){
+//         return true
+//     } else if ( number % 2 === 1){
+//         return false
+//     }
+// }
+
+// let inputNumber=prompt("Digite um número")
+// const passo1 = numeroPar(inputNumber)
+// console.log(`O número ${inputNumber} é par? Resposta: ${passo1}`)
 
   
-  // c
-
-  const array =[]
-  function verificarArray(arrayParametro){
-    for(let number of arrayParametro){
-        if(number % 2 === 0){
-            array.push()
-        }else{
-            return false
-        }
-    }
-    return arrayParametro.length
-  }
-  const resposta= verifircarArray(array)
+//   // c
+  
+  
+  
+//   function verificarArray(lista){
+//     let numPar=[]
+//     for(let elemento of lista){
+//         if(elemento % 2 === 0){
+//             numPar.push(elemento)
+        
+//         }
+//     }
+//     return lista.length 
+//   }
+//   const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+//   const resposta=verificarArray(array)
+//   console.log(resposta)
  
 
+// //d
+
+
+// const numbers =[10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+// let novoArray= []
+
+// for (num of numbers){
+//     if (num %2 === 0){
+//         novoArray= num
+//     }
+//     console.log(novoArray);
+// }
+
+
+// //Desafio 1
+
+// let semParar= () =>{ 
+//  console.log('Teste arrow function');
+// }
+
+
+// let somaNumero=(number1 ,number2) =>{
+//     const soma =number1 + number2
+//     return soma
+// }
+// const resultado2 = somaNumero(7,6)
+// console.log(resultado2)
+
+// //Desafio 2
+// //c
+// const lista = [0, 8, 23, 16, 10, 15, 41, 12, 13]
+// let maior =0
+
+//   for(let i =0 ; i < lista.length; i++){
+//     if(lista[i] > maior){
+//       maior = lista[i]
+//       }
+//   }
+//       console.log(maior)
+
+//       // 
