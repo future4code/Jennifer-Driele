f2894f8f798dcd25a41898765bab09296f49110b
Jennifer Silva <jenni75dr@gmail.com>
2020-08-06 00:01:54 -0300

    Exercícos modificado e parte do desafio feito

--- semana4/objetos/index.js
+++ semana4/objetos/index.js
@@ -1,38 +1,46 @@
+let arrayPost =[]
 
 //Selecionar Dom
-function apertouBotao(){ 
-
-const tituloPost = document.getElementById("titulo-post")
-const autorPost = document.getElementById("autor-post")
-const conteudoPost = document.getElementById("conteudo-post")
-
+     const tituloPost = document.getElementById("titulo-post")
+     const  autorPost = document.getElementById("autor-post")
+     const conteudoPost = document.getElementById("conteudo-post")
+     const imagemPost = document.getElementById("imagem-post")
 
 //Criar Object
 
- const criarPost ={
-     titulo:tituloPost.Value,
-     autor: autorPost.value,
-     conteúdo:conteudoPost.value
- }
- //Adcionar array
- const arrayPost =[]
- arrayPost.push(criarPost)
+let post ={
+    titulo:tituloPost.value,
+    autor: autorPost.value,
+    conteudo:conteudoPost.value,
+    imagem:imagemPost.value
+    }
+//Adcionar array
+ 
+arrayPost.push(post) 
 
- //imprimir no console
-// console.log(criarPost)
-// console.log(arrayPost)
+//imprimir no console
+ console.log(post)
+ console.log(arrayPost)
+
+  // limpar os campos
+  tituloPost.value = ""
+  autorPost.value = ""
+  conteudoPost.value = ""
 
 //post na pagiana html
-const containerPost = document.getElementById("container-de-posts")
-containerPost.innerHtml +=`<div><h2>${tituloPost.value}</h2> <h3>${autorPost.value}</h3> <p>${conteudoPost.value}</p></div>`
 
-
-
-// limpar os campos
-tituloPost.value=""
-autorPost.value=""
-conteudoPost.value=""
+function apertarBotao(){
+    const containerPost = document.getElementById("container-de-posts")
+    containerPost.innerHTML +=`<div><h2>${tituloPost.value}</h2><h3>${autorPost.value}</h3><p>${conteudoPost.value}</p></div>`
+    if (imagemPost.value.includes('.png') || imagemPost.value.includes('.jpg')){
+        containerPost.innerHTML += `<img src=${imagemPost.value}>`
+    }else{
+        alert("Nenhuma imagem adicionada")
+    }
  
 }
+// Função aperta o botão
 
-
+function addPost(event){
+    apertarBotao()
+}
