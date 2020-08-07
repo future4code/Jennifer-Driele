3a00f3de1a8006c94ca0bbcca6e34fd95618db16
Jennifer Silva <jenni75dr@gmail.com>
2020-08-07 13:16:21 -0300

    Exercício de interpretação pronto

---
+++ semana4/lista-de-exrcícios/index.js
@@ -0,0 +1,28 @@
+//Exercíco 1 (De interpretação de código)
+
+// Ele pega o valor em Dolar e multiplica pelo valor da cotação em real, 
+//retornando o valor que o usuario digitar no prompt convertido para real.
+
+//Exercíco 2
+
+// Ele multiplica o valor do seu investimento pelos juros, quando da console.log,
+// ele mostra o valor total já multiplicado , no caso so segundo console , ele vai mostrar undefined pós o valor "Tesouro Direto" , não faz parte
+// da estrutura de codigo.
+
+//Exercício 3
+
+// Primeiro ele vai retornar o total de numeros na araray que são 14
+// no array1 ele retorna todos os numeros que são  Par
+// no array2 retorna todos os numeros Impar
+
+// Exercíco 4
+// numero1 = (infinity) ela vai achar o menor numero do array
+// numero2 = ele vai achar o maior numero do array
+
+//Exercício de Lógica de programação
+// 1
+
+//While, for , for of
+
+// let ListaDeNomes
+
