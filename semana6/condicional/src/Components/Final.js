4703ce7ef19f8aa55c7215b7291a0527e5e21c75
Jennifer Silva <jenni75dr@gmail.com>
2020-08-17 16:39:10 -0300

    Exercícios Renderização Condicional pronto

---
+++ semana6/condicional/src/Components/Final.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Final(props){
+    return(
+        <div>
+            <h2>O Formulário Acabou</h2>
+            <p>Muito Obrigado por Participar! Entraremos em Contato!</p>
+        </div>
+    );
+}
