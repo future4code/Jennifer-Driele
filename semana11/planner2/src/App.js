b20411772a1612e1544652a8567309cb70464090
Jennifer Silva <jenni75dr@gmail.com>
2020-09-24 16:43:43 -0300

    Partes do projeto modificado

--- semana11/planner2/src/App.js
+++ semana11/planner2/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 import Post from './components/Post'
@@ -7,83 +7,76 @@ import axios from 'axios';
 
 const Header = styled.div`
  height: 10vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color:#003380;
-    color: white;
-    font-size:25px ;
-    padding:30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color:#003380;
+  color: white;
+  font-size:25px ;
+  padding:30px;
 
 `
 const Botão = styled.button`
-margin-left:10px;
-border-radius:10px;
-width:80px;
+  margin-left:10px;
+  border-radius:10px;
+  width:80px;
   height:35px;
   background-color:#003380;
   color:white;
 `
 
 function App() {
-  const [tarefa, setTarefa] = useState([])
+  
+const [inputTask, setInputTask] = useState("")
+const [inputDay, setInputDay]= useState("")
+ 
 
+const onChangeDay = (event) =>{
+  setInputDay(event.target.value)
+}
 
-  useEffect(() => {
-    axios
-      .get(`https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-jackson-jennifer`)
-      .then((response) => {
-        setTarefa(response.data)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }, [])
+const onChangeInput = (event) =>{
+  setInputTask(event.target.value)
+}
 
-  const getCreateTarefa = () => {
-    const body = {
-      text: tarefa.text,
-      day: tarefa.day,
-    }
-    axios
-      .post(`https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-jackson-jennifer`, body)
-      .then((response) => {
-        console.log(response.data)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+const addPost =  () => {
+  const body = {
+    text: inputTask,
+    day: inputDay
   }
+  axios
+    .post(`https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-jackson-jennifer`, body)
+    .then((response) => {
+      setInputTask(response.data)
+      console.log(response)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
 
-  return (
-    <div>
-      {/*           
-    {tarefa.map((item)=>{
-        return(
-          <div>{item.text}-{item.day}</div>
-        )
-      // })} */}
-      <Header>
-
-        <p>Nova Tarefa:</p>
-        <input type="text" className="tarefa" />
-        <select name="dias-semana" className="dias-semana">
-          <option value="domingo">Domingo</option>
-          <option value="segunda">Segunda-feira</option>
-          <option value="terca">Terça-feira</option>
-          <option value="quarta">Quarta-feira</option>
-          <option value="quinta">Quinta-feira</option>
-          <option value="sexta">Sexta-feira</option>
-          <option value="sabado">Sábado</option>
-        </select>
-        <Botão onClick={getCreateTarefa}>Criar tarefa</Botão>
-
-      </Header>
-      <Post />
-
-
-    </div>
-  );
+  
+return (
+  <div>
+  <Header>
+<p>Nova Tarefa:</p>
+      <input type="text"  onChange={onChangeInput} className="tarefa" />
+      <select name="dias-semana" onChange={onChangeDay} className="dias-semana">
+        <option value="domingo">Domingo</option>
+        <option value="segunda">Segunda-feira</option>
+        <option value="terca">Terça-feira</option>
+        <option value="quarta">Quarta-feira</option>
+        <option value="quinta">Quinta-feira</option>
+        <option value="sexta">Sexta-feira</option>
+        <option value="sabado">Sábado</option>
+      </select>
+      <Botão onClick={addPost}>Criar tarefa</Botão>
+     
+    </Header>
+    <Post/>
+ </div>
+ 
+);
 }
 
 export default App;
--- semana11/planner2/src/components/Post.js
+++ semana11/planner2/src/components/Post.js
@@ -1,47 +1,108 @@
-import React from 'react';
+import React , {useState,useEffect} from 'react';
 import '../App.css';
-
+import axios from "axios"
 function Post () {
-    return(
-        <div>
- <section className="secao">
-        <div className="semana">
-            <h3>Domingo</h3>
-            <div id="domingo"></div>
-        </div>
 
-        <div className="semana">
-            <h3>Segunda-feira</h3>
-            <div id="segunda"></div>
-        </div>
+     
+const [task, setTask] = useState([])
 
-        <div className="semana">
-            <h3>Terça-feira</h3>
-            <div id="terca"></div>
-        </div>
-    
-        <div className="semana">
-            <h3>Quarta-feira</h3>
-            <div id="quarta"></div>
-        </div>
-    
-        <div className="semana">
-            <h3>Quinta-feira</h3>
-            <div id="quinta"></div>
-        </div>
+const getTasks = () => {
+   axios.get("https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-jackson-jennifer")
 
-        <div className="semana">                    
-            <h3>Sexta-feira</h3>               
-            <div id="sexta"></div>
-        </div>
+     .then((response) => {
+       setTask(response.data)
+     })
+     .catch((err) =>{
+       console.log(err)
+     })
+ }
 
-        <div className="semana">
-            <h3>Sábado</h3>
-            <div id="sabado"></div>
-        </div>
-    </section>
-    
-        </div>
-    )
+ useEffect(() => {
+   getTasks()
+ }, [])
+
+ const deleteTask =(id)=> {
+     axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-jackson-jennifer/${id}`)
+     .then((response)=>{
+        getTasks() 
+     })
+     .catch((err) =>{
+         console.log(err)
+     })
+ }
+//  const tasksDay = task.filter ((item) =>{
+//      return item.day === "Domingo";
+//  })
+
+//  return( 
+//     <div>
+//         <h2>Domingo</h2>
+//         <h3>Total de Atividade:{tasksDay.length}</h3>
+//         {tasksDay.map((element,index) =>{
+//             return( 
+//             <p key={element.id}>
+//                 {index +1}: {element.text || "sem atividade"}
+//                 <button onClick ={() =>deleteTask(element.id)}>x</button>
+//             </p>
+//             )
+//         })}
+//     </div>
+ 
+ return(
+    <div>
+
+<section className="secao">
+         {task.map((element,index) =>{
+            return( 
+             <p key={element.id}>{index +1}: {element.text || "sem atividade"}
+                <button onClick ={() =>deleteTask(element.id)}>x</button>
+            </p>
+            )
+        })}
+
+
+
+
+<div className="semana">
+      <h3>Domingo</h3>
+      <div id="domingo"></div>
+  </div>
+
+  <div className="semana">
+      <h3>Segunda-feira</h3>
+      <div id="segunda"></div>
+  </div>
+
+  <div className="semana">
+      <h3>Terça-feira</h3>
+      <div id="terca"></div>
+  </div>
+
+  <div className="semana">
+      <h3>Quarta-feira</h3>
+      <div id="quarta"></div>
+  </div>
+
+  <div className="semana">
+      <h3>Quinta-feira</h3>
+      <div id="quinta"></div>
+  </div>
+
+  <div className="semana">                    
+      <h3>Sexta-feira</h3>               
+      <div id="sexta"></div>
+  </div>
+
+  <div className="semana">
+      <h3>Sábado</h3>
+      <div id="sabado"></div>
+  </div>
+  
+
+</section>
+
+  </div>
+)
 }
+
 export default Post
