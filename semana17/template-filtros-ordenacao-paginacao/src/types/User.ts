05537cd51b672ddbd4ffcd0ca1375dd39ba50424
Jennifer Silva <jenni75dr@gmail.com>
2020-11-03 16:53:17 -0300

    Exercicios filtro e ordenação

---
+++ semana17/template-filtros-ordenacao-paginacao/src/types/User.ts
@@ -0,0 +1,12 @@
+export type User = {
+   id: string,
+   name: string,
+   email: string,
+   type: string,
+}
+
+export type inputData = {
+   name:string,
+   orderBy:string,
+   orderType: string
+}
